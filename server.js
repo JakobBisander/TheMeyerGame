0403bbf5f4c7ae36174226d086963be620afa94c
Simon Sinding <Sinding2000@gmail.com>
2018-11-07 16:57:30 +0100

    some nice hcnages

--- server.js
+++ server.js
@@ -12,76 +12,84 @@ app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+	response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
 server.listen(5000, function() {
-  console.log('Starting server on port 5000');
+	console.log('Starting server on port 5000');
 });
 
 let players = [];
 let game;
 
 io.on('connection', function(socket) {
-  socket.on('addPlayer', function(data) {
-    const player = new Player(data.name, socket.id);
-    console.log('Adding player: ', player.name);
-    players.push(player);
-    socket.emit('playerJoined', player);
-  });
+	console.log('client with id', socket.id, 'connected');
 
-  socket.on('leave', function() {
-    const leavingPlayer = game.players.filter(player => {
-      return player.socketId === socket.id;
-    });
-    game.removePlayer(leavingPlayer);
-  });
+	socket.on('addPlayer', function(data) {
+		const player = new Player(data.name, socket.id);
+		console.log('Adding player: ', player.name);
+		players.push(player);
+		socket.emit('playerJoined', player);
+	});
 
-  socket.on('disconnect', function() {
-    const disconnectingPlayer = game.players.filter(player => {
-      return player.socketId === socket.id;
-    });
-    game.removePlayer(disconnectingPlayer);
-  });
+	socket.on('leave', function() {
+		const leavingPlayer = game.players.filter(player => {
+			return player.socketId === socket.id;
+		});
+		game.removePlayer(leavingPlayer);
+	});
 
-  socket.on('start', function() {
-    game = new Game(players);
-  });
+	socket.on('disconnect', function(socket) {
+		console.log(socket);
 
-  socket.on('roll', function() {
-    io.to(socket.id).emit('returnRoll', game.rollDice());
-  });
+		// const disconnectingPlayer = game.players.filter(player => {
+		//   return player.socketId === socket.id;
+		// });
+		// game.removePlayer(disconnectingPlayer);
+	});
 
-  socket.on('lift', function() {
-    const playerObject = game.endRound();
+	socket.on('start', function() {
+		if (players.length === 0) {
+			socket.emit('error', { message: 'error not enough players' });
+			return;
+		}
+		game = new Game(players);
+	});
 
-    const nextPlayer = game.getCurrentPlayer();
-    //Noget med nogle emits til clients
-  });
+	socket.on('roll', function() {
+		io.to(socket.id).emit('returnRoll', game.rollDice());
+	});
 
-  socket.on('call', function(data) {
-    const callingPlayer = game.players.filter(player => {
-      return player.socketId === socket.id;
-    });
-    // console.log('Calling player: ', callingPlayer.name);
-    // game.call(data, callingPlayer);
-    const nextPlayer = game.getCurrentPlayer();
+	socket.on('lift', function() {
+		const playerObject = game.endRound();
 
-    const newData = {
-      data,
-      playerName: callingPlayer.name
-    };
-    Object.assign(data, newData);
+		const nextPlayer = game.getCurrentPlayer();
+		//Noget med nogle emits til clients
+	});
 
-    socket.emit('playerCalled', data);
-    io.to(nextPlayer.socketId).emit('yourTurn', data);
-  });
+	socket.on('call', function(data) {
+		const callingPlayer = game.players.filter(player => {
+			return player.socketId === socket.id;
+		});
+		// console.log('Calling player: ', callingPlayer.name);
+		// game.call(data, callingPlayer);
+		const nextPlayer = game.getCurrentPlayer();
 
-  socket.on('lie', function(data) {
-    game.lie(data);
-  });
+		const newData = {
+			data,
+			playerName: callingPlayer.name
+		};
+		Object.assign(data, newData);
 
-  socket.on('risk', function() {
-    game.risk();
-  });
+		socket.emit('playerCalled', data);
+		io.to(nextPlayer.socketId).emit('yourTurn', data);
+	});
+
+	socket.on('lie', function(data) {
+		game.lie(data);
+	});
+
+	socket.on('risk', function() {
+		game.risk();
+	});
 });
--- static/client.js
+++ static/client.js
@@ -1,54 +1,56 @@
-var socket = io();
+const logTextarea = document.getElementById('logTextarea');
+var socket = io('http://localhost:5000');
 
+socket.on('connect', () => {
+	console.log('connected to server');
+	socket.emit('addPlayer', { name: 'Simon Fucking Sinding' });
+});
 //  Listeners
 socket.on('returnRoll', function(data) {
-  console.log(data);
-  // Display roll to player
+	console.log(data);
+	// Display roll to player
 });
 
 socket.on('playerJoined', function(data) {
-  console.log(data);
-  // Add data.name to log
+	console.log(data);
+	logTextarea.value += `${data.name} joined the session`;
+	// Add data.name to log
 });
 
-function endTurn(){
-  document.getElementsByClassName("").style.visibility = "hidden";
+function endTurn() {
+	document.getElementsByClassName('').style.visibility = 'hidden';
 }
 
-document.getElementById("liftButton").addEventListener("click", () =>
-sendLift(0)
-document
- )
+document.getElementById('liftButton').addEventListener('click', () => sendLift(0));
 
 // Emits
 function joinGame(player) {
-  socket.emit('addPlayer', player);
+	socket.emit('addPlayer', player);
 }
 
 function sendStart() {
-  socket.emit('start');
+	socket.emit('start');
 }
 
 function getRoll() {
-  socket.emit('roll');
+	socket.emit('roll');
 }
 
 function sendLift() {
-  socket.emit('lift');
+	socket.emit('lift');
 }
 
 function sendCall(die1, die2) {
-  socket.emit('call', [die1, die2]);
+	socket.emit('call', [die1, die2]);
 }
 
 function sendLie(die1, die2) {
-  socket.emit('lie', [die1, die2]);
+	socket.emit('lie', [die1, die2]);
 }
 
 function sendRisk() {
-  socket.emit('risk');
+	socket.emit('risk');
 }
-
 // setInterval(function() {
 //   socket.emit('movement', movement);
 // }, 1000 / 60);
